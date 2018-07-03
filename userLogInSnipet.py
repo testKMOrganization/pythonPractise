757137c41295f96d222b5a0d52c68d834b9bf283
ffoss <ffossrules@gmail.com>
2018-07-03 23:04:54 +0200

    Update userLogInSnipet.py

--- userLogInSnipet.py
+++ userLogInSnipet.py
@@ -3,15 +3,28 @@
 #Takes user input and checks conditions and then proceeds#
 #Does the similar actions such as the previous commit but using functions#
 
+#User name and user password now declared as global#
 userName = 'Kabir'
 userPassword = '1@K.P'
 
-print ('Input user name ')
-uName = input('')
-print ('Input user password ')
-uPassword = input()
 
-if (uName == userName and uPassword == userPassword):
-    print('Now you can proceed ')
-else:
-   print('Who the hell are you?')
+
+def userNameInput():
+    print ('Input user name ')
+    uName = input('')
+    return uName
+def userPassInput():
+    print ('Input user password ')
+    uPassword = input()
+    return uPassword
+
+def checkCredentials(uName, uPassword):
+    if (uName == userName and uPassword == userPassword):
+        print('Now you can proceed ')
+    else:
+        print('Who the hell are you?')
+
+
+uName = userNameInput()
+uPassword = userPassInput()
+checkCredentials(uName, uPassword)
