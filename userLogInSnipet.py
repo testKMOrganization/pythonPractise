af5679f9652ecfe0557e85e9f2bb03428a6de079
ffoss <ffossrules@gmail.com>
2018-07-03 23:08:07 +0200

    uses a data storage
    A data storage added

--- userLogInSnipet.py
+++ userLogInSnipet.py
@@ -1,11 +1,20 @@
 #My first Python Program#
 
-#Third version#
+
+
+#Fourth Patch#
+
 # Checks the credentials and if wrong tells the user which input was wrong and asks for new input#
 
+# Added: uses a data storage
 
-userName = 'Kabir'
-userPassword = '1@K.P'
+
+dataStorage = {
+    "userName": "Kabir",
+    "userPassword": "1@K.P"
+    }
+
+
 
 
 def userNameInput():
@@ -19,18 +28,18 @@ def userPassInput():
     return uPassword
 
 def checkCredentials(uName, uPassword):
-    if (uName == userName and uPassword == userPassword):
+    if (uName == dataStorage["userName"] and uPassword == dataStorage["userPassword"]):
         print('Now you can proceed ')
         
-    elif (uName == userName and uPassword != userPassword):
+    elif (uName == dataStorage["userName"] and uPassword != dataStorage["userPassword"]):
         print('You have given a wrong password ')
         uPassword = userPassInput()
         checkCredentials(uName, uPassword)
-    elif (uName != userName and uPassword == userPassword):
+    elif (uName != dataStorage["userName"] and uPassword == dataStorage["userPassword"]):
         print('You have given wrong user name ')
         uName = userNameInput()
         checkCredentials(uName, uPassword)
-    elif (uName != userName and uPassword != userPassword):
+    elif (uName != dataStorage["userName"] and uPassword != dataStorage["userPassword"]):
         print('Both your user name and password is incorrect ')
         uName = userNameInput()
         uPassword = userPassInput()
