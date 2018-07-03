51892eeba006e721941f8fcb46dcdb5d8f5c5039
ffoss <ffossrules@gmail.com>
2018-07-03 23:02:50 +0200

    Using function
    #Does the similar actions such as the previous commit but using functions

--- userLogInSnipet.py
+++ userLogInSnipet.py
@@ -1,6 +1,7 @@
 #My first Python Program#
 
 #Takes user input and checks conditions and then proceeds#
+#Does the similar actions such as the previous commit but using functions#
 
 userName = 'Kabir'
 userPassword = '1@K.P'
