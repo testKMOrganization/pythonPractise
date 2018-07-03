ed069613e3fd5b4d88801b26f147dd301998c098
ffoss <ffossrules@gmail.com>
2018-07-03 23:10:39 +0200

    Update userLogInSnipet.py
    Added: User will set user name and pass word

--- userLogInSnipet.py
+++ userLogInSnipet.py
@@ -2,19 +2,18 @@
 
 
 
-#Fourth Patch#
+#5th
+#Add: User will set user name and password
+#User will input user name and password and the program will check
+#Everything else like before
 
-# Checks the credentials and if wrong tells the user which input was wrong and asks for new input#
 
-# Added: uses a data storage
-
+globalCounter = 0
 
 dataStorage = {
-    "userName": "Kabir",
-    "userPassword": "1@K.P"
+    "userName": "",
+    "userPassword": ""
     }
-
-
 
 
 def userNameInput():
@@ -27,27 +26,57 @@ def userPassInput():
     uPassword = input()
     return uPassword
 
+def checkAttempts (i):
+    if i > 3:
+        print('You have attempted with wrong username or password too many times. Your account is temporary blocked. Try again later! ')
+        quit()
+
+def setUserCredentials():
+    dataStorage["userName"] = userNameInput()
+    dataStorage["userPassword"] = userPassInput()
+
 def checkCredentials(uName, uPassword):
+
+    global globalCounter
+    
+    #if correct
     if (uName == dataStorage["userName"] and uPassword == dataStorage["userPassword"]):
         print('Now you can proceed ')
+
+    #if wrong
         
     elif (uName == dataStorage["userName"] and uPassword != dataStorage["userPassword"]):
+        
+        globalCounter = globalCounter + 1
+        checkAttempts (globalCounter)
+        
         print('You have given a wrong password ')
         uPassword = userPassInput()
         checkCredentials(uName, uPassword)
     elif (uName != dataStorage["userName"] and uPassword == dataStorage["userPassword"]):
+        
+        globalCounter = globalCounter + 1
+        checkAttempts (globalCounter)
+        
         print('You have given wrong user name ')
         uName = userNameInput()
         checkCredentials(uName, uPassword)
     elif (uName != dataStorage["userName"] and uPassword != dataStorage["userPassword"]):
+        
+        globalCounter = globalCounter + 1
+        checkAttempts (globalCounter)
+        
         print('Both your user name and password is incorrect ')
         uName = userNameInput()
         uPassword = userPassInput()
         checkCredentials(uName, uPassword)
+        
+    #could there be any other combinations of fails?    
     else:
         print('What'+'s'+' going on?')
 
-
+setUserCredentials()
+print('Now try to log in! ')
 uName = userNameInput()
 uPassword = userPassInput()
 checkCredentials(uName, uPassword)
